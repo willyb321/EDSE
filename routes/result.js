2104dde105a825672b082df51b70a0f8420db4be
willyb321 <wbwilliam7@gmail.com>
2017-06-22 11:38:56 +1000

    data update, multiselect + more

--- routes/result.js
+++ routes/result.js
@@ -10,29 +10,33 @@ let names = Object.keys(data[0]);
 /* GET home page. */
 router.get('/:mat/:system', function(req, res, next) {
 	// console.log(vals.length);
-	request(`http://elitebgs.kodeblox.com/api/populatedsystems/name/${req.params.system}/statename/`, (error, response, body) => {
-		console.log('error:', error); // Print the error if one occurred
-		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-		console.log('body:', body);
-		body = JSON.parse(body);
-	});
+	// request(`http://elitebgs.kodeblox.com/api/populatedsystems/name/${req.params.system}/statename/`, (error, response, body) => {
+		// console.log('error:', error); // Print the error if one occurred
+		// console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		// console.log('body:', body);
+		// body = JSON.parse(body);
+	// });
+	let mats = req.params.mat.split(',');
+	console.log(mats)
 	let vals = [];
 	_.each(data, (elem, ind) => {
-		if (elem.Material === req.params.mat) {
-			vals.push(elem)
-		}
+		_.each(mats, mat => {
+			if (elem.Material === mat) {
+				vals.push(elem)
+			}
+		})
 	});
 	let datas = [];
 	datas[0] = names;
 	if (!_.isEmpty(vals)) {
 		datas[1] = vals;
 	}
-	request(`http://elitebgs.kodeblox.com/api/populatedsystems/name/${req.params.system}/statename/`, (error, response, body) => {
-		console.log('error:', error); // Print the error if one occurred
-		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-		console.log('body:', body);
-		body = JSON.parse(body);
-	});
+	// request(`http://elitebgs.kodeblox.com/api/populatedsystems/name/${req.params.system}/statename/`, (error, response, body) => {
+		// console.log('error:', error); // Print the error if one occurred
+		// console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		// console.log('body:', body);
+		// body = JSON.parse(body);
+	// });
 	res.render('Result', { title: 'EDSE', result: datas});
 });
 
