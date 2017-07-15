a91b33e1624b4e1e695f207f6798a97505f46917
willyb321 <wbwilliam7@gmail.com>
2017-07-15 15:59:55 +1000

    remove unused bits

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const _ = require('lodash');
 const data = require('../public/data.json');
 const getIndexes = require('./getindexes');
 
 const vals = getIndexes();
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
 	res.render('index', {title: 'EDSE', data: vals, names: vals});
 });
 
