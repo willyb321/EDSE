fd6ae1c7dbe34b5f0e1d369b2f0bcdcd41a61ffc
willyb321 <wbwilliam7@gmail.com>
2017-07-19 14:45:12 +1000

    Yay I think its working now.

--- routes/result.js
+++ routes/result.js
@@ -18,7 +18,7 @@ function removeColumn(data, columnNames) {
 
 process.on('unhandledRejection', err => {
 	console.log(err.stack)
-})
+});
 
 router.get('/', (req, res) => {
 	res.status(418);
@@ -36,14 +36,14 @@ function distanceGet(x1, y1, z1, x2, y2, z2) {
 function systemGet(mats, system) {
 	return new Promise((resolve, reject) => {
 		let toFind = [];
-		_.each(mats, elem => {
-			let toPush = _.findKey(data, (elemtf) => {
-				return elemtf.Material === elem;
-			});
-			console.log(toPush);
-			toFind.push(toPush);
+		let toPush = _.findKey(data, (elemtf) => {
+			return elemtf.Material === mats;
 		});
-		// console.log(toFind);
+		toFind.push(toPush);
+		let toRes = [];
+		if (system === null || system === undefined) {
+			resolve(['N/A', mats])
+		}
 		_.each(toFind, mat => {
 			let params = {};
 			if (data[mat]['System Allegiance']) {
@@ -58,9 +58,8 @@ function systemGet(mats, system) {
 			if (data[mat]['System Economy']) {
 				params.primaryeconomyname = data[mat]['System Economy'];
 			}
-			console.log(params);
+			console.log('Params: ' + JSON.stringify(params));
 			let systemPos = [{}, {}];
-			console.log(system);
 			let allreqs = [axios.get(`http://guest:secret@elitebgs.kodeblox.com/api/eddb/v1/populatedsystems?name=${system}`)];
 			if (!_.isEmpty(params)) {
 				allreqs[1] = axios.get(`http://guest:secret@elitebgs.kodeblox.com/api/eddb/v1/populatedsystems`, {
@@ -83,32 +82,43 @@ function systemGet(mats, system) {
 								});
 								distancesNum.push(distanceGet(parseInt(elem.x), parseInt(elem.y), parseInt(elem.z), parseInt(response.data[0].x), parseInt(response.data[0].y), parseInt(response.data[0].z)));
 							});
-							console.log(distancesNum);
 							let minDis = _.min(distancesNum);
 							const sysToUse = _.findIndex(distances, dis => {
 								return dis.distance === minDis
 							});
 							if (resdata.data.length > 0) {
+								console.log(response.data.length + ' datas');
 								systemPos[0].x = resdata.data[sysToUse].x;
 								systemPos[0].y = resdata.data[sysToUse].y;
 								systemPos[0].z = resdata.data[sysToUse].z;
 								systemPos[0].name = resdata.data[sysToUse].name;
 							}
-							console.log(response.data.length + ' datas');
 							if (response.data.length > 0) {
 								systemPos[1].x = response.data[0].x;
 								systemPos[1].y = response.data[0].y;
 								systemPos[1].z = response.data[0].z;
 								systemPos[1].name = response.data[0].name;
-								resolve([response.data, mat, systemPos]);
+								toRes.push(response.data);
+								toRes.push(mat);
+								toRes.push(systemPos);
 							}
 						}
 					} else {
-						resolve(['N/A', mat])
+						console.log('Nothing found, material was: ' + mat);
+						toRes = ['N/A', mat]
 					}
+					resolve(toRes);
 				}));
-		})
+		});
 	})
+}
+
+function allSys(mats, system) {
+	let allSystems = [];
+	mats.forEach(elem => {
+		allSystems.push(systemGet(elem, system));
+	});
+	return allSystems;
 }
 
 router.get('/:mat/:system?', async (req, res) => {
@@ -119,87 +129,91 @@ router.get('/:mat/:system?', async (req, res) => {
 		mats[ind] = matVals[elem].mat;
 	});
 	mats = _.uniq(mats);
-	const vals = [];
-	if (req.params.system) {
-		const resdata = await systemGet(mats, req.params.system);
-		if (resdata[0] !== 'N/A') {
-			let distance = distanceGet(parseInt(resdata[2][0].x), parseInt(resdata[2][0].y), parseInt(resdata[2][0].z), parseInt(resdata[2][1].x), parseInt(resdata[2][1].y), parseInt(resdata[2][1].z)).toFixed(2);
-			console.log(distance);
-			data[resdata[1]].System = `${resdata[2][0].name} (${distance}ly away from ${resdata[2][1].name})`
-		} else {
-			data[resdata[1]].System = resdata[0]
-		}
-	}
-	_.each(data, elem => {
-		_.each(mats, mat => {
-			if (elem.Material === mat) {
-				vals.push(elem);
+	let allSystems = allSys(mats, req.params.system);
+	Promise.all(allSystems).then(results => {
+		_.each(results, resdata => {
+			if (resdata[0] !== 'N/A' && resdata[2]) {
+				console.log(resdata[2]);
+				let distance = distanceGet(parseInt(resdata[2][0].x), parseInt(resdata[2][0].y), parseInt(resdata[2][0].z), parseInt(resdata[2][1].x), parseInt(resdata[2][1].y), parseInt(resdata[2][1].z)).toFixed(2);
+				console.log('distance: ' + distance);
+				data[resdata[1]].System = `${resdata[2][0].name} (${distance}ly away from ${resdata[2][1].name})`
+			} else {
+				data[resdata[1]].System = resdata[0];
 			}
 		});
-	});
-	const valsMapped = _.each(vals, (item, index, vals) => {
-		const keysShip = item['Ship Type'].split(',');
-		const keysUSS = item.Source.split(',');
-		let popkeys = item['System Population'];
-		const usskeys = [];
-		const shipkeys = [];
-		_.each(keysShip, elem => {
-			elem = elem.trim();
-			try {
-				if (!_.hasIn(humanNames['Ship Type'], vals[index]['Ship Type']) && _.findKey(humanNames['Ship Type'], elem)) {
-					const newVal = humanNames['Ship Type'][_.findKey(humanNames['Ship Type'], elem)][elem];
-					shipkeys.push(newVal);
-					// console.log('Ship newVal: ' + newVal);
-				} else {
-					shipkeys.push(elem);
-					// console.log('Ship newVal: ' + newVal);
+	}).then(() => {
+		const vals = [];
+		_.each(data, elem => {
+			_.each(mats, mat => {
+				if (elem.Material === mat) {
+					vals.push(elem);
 				}
-			} catch (err) {
-				console.log(err);
-			}
+			});
 		});
-		switch (popkeys) {
-			case 'Small':
-				popkeys = humanNames.Population[_.findKey(humanNames.Population, 'Small')]['Small'];
-				break;
-			case 'Medium':
-				popkeys = humanNames.Population[_.findKey(humanNames.Population, 'Medium')]['Medium'];
-				break;
-			case 'High':
-				popkeys = humanNames.Population[_.findKey(humanNames.Population, 'High')]['High'];
-				break;
-			default:
-				break;
-		}
-		item['System Population'] = popkeys;
-		_.each(keysUSS, elem => {
-			elem = elem.trim();
-			try {
-				if (!_.hasIn(humanNames.Source, vals[index].Source) && _.findKey(humanNames.Source, elem)) {
-					const newVal = humanNames.Source[_.findKey(humanNames.Source, elem)][elem];
-					usskeys.push(newVal);
-					// console.log('USS newVal: ' + newVal);
-				} else {
-					usskeys.push(elem);
-					// console.log('USS newVal: ' + newVal);
+		const valsMapped = _.each(vals, (item, index, vals) => {
+			const keysShip = item['Ship Type'].split(',');
+			const keysUSS = item.Source.split(',');
+			let popkeys = item['System Population'];
+			const usskeys = [];
+			const shipkeys = [];
+			_.each(keysShip, elem => {
+				elem = elem.trim();
+				try {
+					if (!_.hasIn(humanNames['Ship Type'], vals[index]['Ship Type']) && _.findKey(humanNames['Ship Type'], elem)) {
+						const newVal = humanNames['Ship Type'][_.findKey(humanNames['Ship Type'], elem)][elem];
+						shipkeys.push(newVal);
+						// console.log('Ship newVal: ' + newVal);
+					} else {
+						shipkeys.push(elem);
+						// console.log('Ship newVal: ' + newVal);
+					}
+				} catch (err) {
+					console.log(err);
 				}
-			} catch (err) {
-				console.log(err);
+			});
+			switch (popkeys) {
+				case 'Small':
+					popkeys = humanNames.Population[_.findKey(humanNames.Population, 'Small')]['Small'];
+					break;
+				case 'Medium':
+					popkeys = humanNames.Population[_.findKey(humanNames.Population, 'Medium')]['Medium'];
+					break;
+				case 'High':
+					popkeys = humanNames.Population[_.findKey(humanNames.Population, 'High')]['High'];
+					break;
+				default:
+					break;
 			}
+			item['System Population'] = popkeys;
+			_.each(keysUSS, elem => {
+				elem = elem.trim();
+				try {
+					if (!_.hasIn(humanNames.Source, vals[index].Source) && _.findKey(humanNames.Source, elem)) {
+						const newVal = humanNames.Source[_.findKey(humanNames.Source, elem)][elem];
+						usskeys.push(newVal);
+						// console.log('USS newVal: ' + newVal);
+					} else {
+						usskeys.push(elem);
+						// console.log('USS newVal: ' + newVal);
+					}
+				} catch (err) {
+					console.log(err);
+				}
+			});
+			vals[index].Source = usskeys.join(', ');
+			vals[index]['Ship Type'] = shipkeys.join(', ');
 		});
-		vals[index].Source = usskeys.join(', ');
-		vals[index]['Ship Type'] = shipkeys.join(', ');
-	});
-	const datas = [];
-	datas[0] = names;
-	if (!_.isEmpty(vals)) {
-		datas[1] = valsMapped;
-	}
-	res.render('Result', {
-		title: 'EDSE',
-		result: datas,
-		meta: `EDSE - ${mats.join(', ')}`
-	});
+		const datas = [];
+		datas[0] = names;
+		if (!_.isEmpty(vals)) {
+			datas[1] = valsMapped;
+		}
+		res.render('Result', {
+			title: 'EDSE',
+			result: datas,
+			meta: `EDSE - ${mats.join(', ')}`
+		});
+	})
 });
 
 module.exports = router;
